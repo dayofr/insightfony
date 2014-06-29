8bac208d93edc9daa504f715bf0642b5628d57a7
Dayo <dayo@dayo.fr>
2014-06-29 12:41:50 +0200

    Fisrt Commit

---
+++ src/Dayo/BlogBundle/DayoBlogBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dayo\BlogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DayoBlogBundle extends Bundle
+{
+}
---
+++ src/Dayo/BlogBundle/Form/PostType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Dayo\BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PostType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('content')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Dayo\BlogBundle\Entity\Post'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'dayo_blogbundle_post';
+    }
+}
