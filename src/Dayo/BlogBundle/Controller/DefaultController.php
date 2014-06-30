dfbb1b1b273cf629ae7ac65c59cc46fafaa6f669
dayofr <dayo@dayo.fr>
2014-06-30 13:31:35 +0200

    Update DefaultController.php

--- src/Dayo/BlogBundle/Controller/DefaultController.php
+++ src/Dayo/BlogBundle/Controller/DefaultController.php
@@ -5,11 +5,13 @@ namespace Dayo\BlogBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/hello/{name}")
+     * @Method("GET")
      * @Template()
      */
     public function indexAction($name)
